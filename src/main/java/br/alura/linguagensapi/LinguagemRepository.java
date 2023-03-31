af1deddfda9dfac06c66e11fefd2dd9470c95ac8
olgajuanne <olgajdango@gmail.com>
2023-03-31 19:14:13 -0300

    first commit

---
+++ src/main/java/br/alura/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,7 @@
+package br.alura.linguagensapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+  
+}
